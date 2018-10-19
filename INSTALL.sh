842473cfe7ce6ab7e4af78c9a844bd5a0b8a4baf
Jeroen Rijken <jeroen.rijken@gmail.com>
2018-10-19 15:53:27 +0200

    Update the install script and modify the logstash input directory.

--- INSTALL.sh
+++ INSTALL.sh
@@ -10,9 +10,10 @@ setcap cap_net_raw,cap_net_admin=eip /usr/bin/capstats
 
 # Update the log directory permissions
 chown -R logstash:logstash /var/log/bro/
+chown -R logstash:logstash /var/spool/bro/
 
 # Install the bro configuration files
-su logstash -c /usr/bin/broctl install
+sudo -u logstash /usr/bin/broctl install
 
 # Run bro
-#su logstash -c /usr/bin/broctl start
+#sudo -u logstash /usr/bin/broctl start
